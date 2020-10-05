855fd328f98b2c3c0addf011c0269863405f2890
Murray Tait <death.tumble@users.noreply.github.com>
2020-10-05 12:57:57 +0100

    Use new web module

---
+++ src/main/terraform/web.tf
@@ -0,0 +1,30 @@
+locals {
+  config = {
+    REACT_APP_API_ENDPOINT                     = "https://${var.application_name}-api.${module.common.fqdn_no_app}/query"
+    REACT_APP_PRIMARY_SLDN                     = module.common.fqdn_no_app
+    REACT_APP_API_SLDN                         = module.common.fqdn_no_app
+    REACT_APP_SSO_COOKIE_SLDN                  = module.common.fqdn_no_app
+    REACT_APP_AWS_COGNITO_REGION               = module.common.aws_region
+    REACT_APP_AWS_COGNITO_IDENTITY_POOL_REGION = module.common.aws_region
+    REACT_APP_AWS_COGNITO_AUTH_FLOW_TYPE       = "USER_SRP_AUTH"
+    REACT_APP_AWS_COGNITO_COOKIE_EXPIRY_MINS   = tonumber(55)
+    REACT_APP_AWS_COGNITO_COOKIE_SECURE        = tobool(true)
+  }
+  app_config = {
+    REACT_APP_API_ENDPOINT                     = "https://${var.application_name}-api.${module.common.fqdn_no_app}/query"
+  }
+}
+
+module "web" {
+  source       = "git@github.com:deathtumble/terraform_modules.git//modules/web?ref=v0.1.23"
+  aws_profile  = module.common.aws_profile
+  fqdn         = "web${module.common.fqdn}"
+  fqdn_no_app  = module.common.fqdn_no_app
+  web_acl_name = module.common.web_acl_name
+  providers = {
+    aws.global = aws.global
+  }
+  environment_config = merge(local.config,
+    local.app_config
+  )
+}

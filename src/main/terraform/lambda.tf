ecc910c77e697fe185dfd1878e5b0ae1bfe428a8
Murray Tait <death.tumble@users.noreply.github.com>
2020-10-31 17:54:57 +0000

    moving lamdba from own to environment api gateway

--- src/main/terraform/lambda.tf
+++ src/main/terraform/lambda.tf
@@ -12,9 +12,9 @@ resource "aws_iam_policy" "lambda_log_access" {
         {
           "Action": [
             "logs:PutLogEvents",
-            "logs:CreateLogGroup"
+            "logs:CreateLogStream"
           ],
-          "Resource": "arn:aws:logs:${module.common.aws_region}:${module.common.aws_account_id}:${aws_lambda_function.main.function_name}",
+          "Resource": "arn:aws:logs:${module.common.aws_region}:${module.common.aws_account_id}:log-group:/aws/lambda/${aws_lambda_function.main.function_name}:*",
           "Effect": "Allow"
         },
         {
@@ -55,3 +55,55 @@ resource "aws_cloudwatch_log_group" "lambda" {
   name              = "/aws/lambda/${aws_lambda_function.main.function_name}"
   retention_in_days = 14
 }
+
+#######################################################################
+
+resource "aws_api_gateway_deployment" "main" {
+  rest_api_id = data.aws_api_gateway_rest_api.external_api.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_integration" "releasable" {
+  rest_api_id = data.aws_api_gateway_rest_api.external_api.id
+  resource_id = aws_api_gateway_resource.releasable.id
+
+  http_method = aws_api_gateway_method.releasable.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.main.invoke_arn
+  content_handling        = null
+
+  depends_on = [
+      data.aws_api_gateway_rest_api.external_api
+  ]
+}
+
+resource "aws_api_gateway_resource" "releasable" {
+  rest_api_id = data.aws_api_gateway_rest_api.external_api.id
+  parent_id   = data.aws_api_gateway_rest_api.external_api.root_resource_id
+  path_part   = "releaseable"
+}
+
+resource "aws_api_gateway_method" "releasable" {
+  rest_api_id      = data.aws_api_gateway_rest_api.external_api.id
+  resource_id      = aws_api_gateway_resource.releasable.id
+  http_method      = "ANY"
+  authorization    = "NONE"
+  api_key_required = false
+}
+
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowAPIGatewayToInvokeLambda"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API.
+  source_arn = "${data.aws_api_gateway_rest_api.external_api.execution_arn}/*/*/*"
+}
+
--- src/main/terraform/project.tf
+++ src/main/terraform/project.tf
@@ -3,38 +3,41 @@ locals {
 }
 
 module "common" {
-  source = "git@github.com:deathtumble/terraform_modules.git//modules/common?ref=v0.1.42"
-#  source           = "../../../../../infra2/terraform/modules/common"
+  source = "git@github.com:deathtumble/terraform_modules.git//modules/common?ref=v0.1.43"
+  #  source           = "../../../../../infra2/terraform/modules/common"
   application_name = local.application_name
   project_name     = "urbanfortress"
   tld              = "uk"
 }
 
 module "lambda_pipeline" {
-  source                                     = "git@github.com:deathtumble/terraform_modules.git//modules/lambda_pipeline?ref=v0.1.42"
-#  source                         = "../../../../../infra2/terraform/modules/lambda_pipeline"
-  application_name                           = local.application_name
-  destination_builds_bucket_name             = module.common.destination_builds_bucket_name
-  lambdas                                    = [aws_lambda_function.main]
+  source = "git@github.com:deathtumble/terraform_modules.git//modules/lambda_pipeline?ref=v0.1.43"
+  #  source                         = "../../../../../infra2/terraform/modules/lambda_pipeline"
+  application_name                          = local.application_name
+  destination_builds_bucket_name            = module.common.destination_builds_bucket_name
+  lambdas                                   = [aws_lambda_function.main]
   aws_sns_topic_env_build_notification_name = module.common.aws_sns_topic_env_build_notification_name
-  aws_account_id                            = module.common.aws_account_id  
+  aws_account_id                            = module.common.aws_account_id
 }
 
-module "api_gateway" {
-  source       = "git@github.com:deathtumble/terraform_modules.git//modules/api_gateway?ref=v0.1.42"
-#  source                         = "../../../../../infra2/terraform/modules/api_gateway"
-  aws_region   = module.common.aws_region
-  aws_profile  = module.common.aws_profile
-  fqdn         = module.common.fqdn
-  fqdn_no_app  = module.common.fqdn_no_app
-  zone_id      = data.aws_route53_zone.environment.zone_id
-  lambda       = aws_lambda_function.main
-  web_acl_name = module.common.web_acl_name
-  providers = {
-    aws.global = aws.global
-  }
+data "aws_wafregional_web_acl" "main" {
+  name = module.common.web_acl_name
 }
 
 data "aws_route53_zone" "environment" {
   name = module.common.fqdn_no_app
+}
+
+data "aws_api_gateway_rest_api" "external_api" {
+  name = "api.${module.common.fqdn_no_app}"
+}
+
+
+data "aws_acm_certificate" "main" {
+  domain   = "*.${module.common.fqdn_no_app}"
+  types    = ["AMAZON_ISSUED"]
+  statuses = ["ISSUED"]
+  provider = aws.global
+
+  most_recent = true
 }

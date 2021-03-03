7ab04ad4a340623eb7cf91121b70d8f4c9eadba8
Murray Tait <6074789+deathtumble@users.noreply.github.com>
2021-03-03 20:47:07 +0000

    Use assume role in terraform buildspec

--- src/main/terraform/project.tf
+++ src/main/terraform/project.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "common" {
-  source = "git@github.com:deathtumble/terraform_modules.git//modules/common?ref=v0.2.5"
+  source = "git@github.com:deathtumble/terraform_modules.git//modules/common?ref=v0.2.6"
   # source           = "../../../../../infra2/terraform/modules/common"
   application_name  = local.application_name
   project_name      = "urbanfortress"
--- src/main/terraform/providers.tf
+++ src/main/terraform/providers.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region  = "eu-west-1"
   profile = module.common.all.aws_profile
+  #assume_role { role_arn = "arn:aws:iam::481652375433:role/releasable-target-terraform-pipleine-CodeBuildRole" }
 }
 
 provider "template" {
@@ -10,6 +11,7 @@ provider "aws" {
   alias   = "global"
   region  = "us-east-1"
   profile = module.common.all.aws_profile
+  #assume_role { role_arn = "arn:aws:iam::481652375433:role/releasable-target-terraform-pipleine-CodeBuildRole" }
 }
 
 provider "archive" {
@@ -18,4 +20,5 @@ provider "archive" {
 provider "myaws" {
   region  = "eu-west-1"
   profile = "${var.master_account_id}_ListAccountsAccess"
+  #assume_role { role_arn = "arn:aws:iam::481652375433:role/OrganizationAccountAccessRole" }
 }

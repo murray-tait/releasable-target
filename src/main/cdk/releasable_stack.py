2ccb24595c790465a75f382a51a5d0a94c013c8d
Murray Tait <murray.tait@infinityworks.com>
2022-02-16 17:49:44 +0000

    remove environment guard

--- src/main/cdk/releasable_stack.py
+++ src/main/cdk/releasable_stack.py
@@ -28,189 +28,188 @@ class ReleasableStack(TerraformStack):
 
         self.environment = get_environment(scope, ns)
 
-        if self.environment != "default":
-            config = Config(scope, ns)
-            self.accounts = Accounts(self.environment, config.accounts_profile)
-            self.shared = Shared(config, self.accounts, self.environment)
+        config = Config(scope, ns)
+        self.accounts = Accounts(self.environment, config.accounts_profile)
+        self.shared = Shared(config, self.accounts, self.environment)
 
-            self._create_backend(config)
-            self._provider_factory = ProviderFactory(
-                self, config, self.shared.aws_role_arn, self.shared.aws_profile)
+        self._create_backend(config)
+        self._provider_factory = ProviderFactory(
+            self, config, self.shared.aws_role_arn, self.shared.aws_profile)
 
-            self._provider_factory.build("eu-west-1")
+        self._provider_factory.build("eu-west-1")
 
-            self.aws_global_provider = self._provider_factory.build(
-                "us-east-1", "global_aws", "global")
+        self.aws_global_provider = self._provider_factory.build(
+            "us-east-1", "global_aws", "global")
 
-            aws_wafregional_web_acl_main = DataAwsWafregionalWebAcl(
-                self,
-                id="main",
-                name=self.shared.web_acl_name
-            )
+        aws_wafregional_web_acl_main = DataAwsWafregionalWebAcl(
+            self,
+            id="main",
+            name=self.shared.web_acl_name
+        )
 
-            route_53_zone = DataAwsRoute53Zone(
-                self,
-                id="environment",
-                name=self.shared.environment_domain_name
-            )
+        route_53_zone = DataAwsRoute53Zone(
+            self,
+            id="environment",
+            name=self.shared.environment_domain_name
+        )
 
-            lambda_service_role = IamRole(
-                self,
-                id="lambda_service_role",
-                name=f'{config.app_name}-lambda-executeRole',
-                assume_role_policy=file("policies/lambda_service_role.json")
-            )
+        lambda_service_role = IamRole(
+            self,
+            id="lambda_service_role",
+            name=f'{config.app_name}-lambda-executeRole',
+            assume_role_policy=file("policies/lambda_service_role.json")
+        )
 
-            lambda_function = LambdaFunction(
-                scope=self,
-                id="lambda",
-                function_name=config.app_name,
-                runtime="provided",
-                handler="bootstrap",
-                timeout=30,
-                role=lambda_service_role.arn,
-                s3_bucket=self.shared.destination_builds_bucket_name,
-                s3_key=f'builds/{config.app_name}/refs/branch/{self.environment}/lambda.zip'
-            )
+        lambda_function = LambdaFunction(
+            scope=self,
+            id="lambda",
+            function_name=config.app_name,
+            runtime="provided",
+            handler="bootstrap",
+            timeout=30,
+            role=lambda_service_role.arn,
+            s3_bucket=self.shared.destination_builds_bucket_name,
+            s3_key=f'builds/{config.app_name}/refs/branch/{self.environment}/lambda.zip'
+        )
 
-            TerraformHclModule(
-                self,
-                id="lambda_pipeline",
-                source="git@github.com:deathtumble/terraform_modules.git//modules/lambda_pipeline?ref=v0.4.2",
-                # source="../../../../terraform/modules/lambda_pipeline",
-                variables={
-                    "application_name": config.app_name,
-                    "destination_builds_bucket_name": self.shared.destination_builds_bucket_name,
-                    "function_names": [lambda_function.function_name],
-                    "function_arns": [lambda_function.arn],
-                    "aws_sns_topic_env_build_notification_name": self.shared.aws_sns_topic_env_build_notification_name,
-                    "aws_account_id": self.accounts.aws_account_id
-                }
-            )
-
-            acm_cert = environment_certificate(
-                self, self.aws_global_provider, self.shared.environment_domain_name
-            )
-
-            api_gateway_module = TerraformHclModule(
-                self,
-                id="api_gateway",
-                source="git@github.com:deathtumble/terraform_modules.git//modules/api_gateway?ref=v0.4.2",
-                # source="../../../../terraform/modules/api_gateway",
-                variables={
-                    "aws_region": config.aws_region,
-                    "fqdn": self.shared.fqdn,
-                    "zone_id": route_53_zone.id,
-                    "web_acl_id": aws_wafregional_web_acl_main.id,
-                    "certificate_arn": acm_cert.arn,
-                    "lambda_invoke_arn": lambda_function.invoke_arn
-                }
-            )
-
-            terraform_build_artifact_key = f'builds/{config.app_name}/refs/branch/{self.environment}/terraform.zip'
-            TerraformLocal(self, "terraform_build_artifact_key",
-                           terraform_build_artifact_key)
-            build_artifact_key = f'builds/{config.app_name}/refs/branch/{self.environment}/cloudfront.zip'
-            TerraformLocal(self, "build_artifact_key", build_artifact_key)
-
-            TerraformHclModule(
-                self,
-                id="cloudfront_pipeline",
-                source="git@github.com:deathtumble/terraform_modules.git//modules/cloudfront_pipeline?ref=v0.1.42",
-                variables={
-                    "fqdn": f'web{self.shared.fqdn}',
-                    "destination_builds_bucket_name": self.shared.destination_builds_bucket_name,
-                    "application_name": "releasable",
-                    "branch_name": self.environment,
-                    "artifacts_bucket_name": self.shared.artifacts_bucket_name,
-                    "build_artifact_key": build_artifact_key
-                }
-            )
-
-            web_config = {
-                "REACT_APP_API_ENDPOINT": f"https://releasable.{self.shared.environment_domain_name}/query",
-                "REACT_APP_PRIMARY_SLDN": self.shared.environment_domain_name,
-                "REACT_APP_API_SLDN": self.shared.environment_domain_name,
-                "REACT_APP_SSO_COOKIE_SLDN": self.shared.environment_domain_name,
-                "REACT_APP_AWS_COGNITO_REGION": config.aws_region,
-                "REACT_APP_AWS_COGNITO_IDENTITY_POOL_REGION": config.aws_region,
-                "REACT_APP_AWS_COGNITO_AUTH_FLOW_TYPE": "USER_SRP_AUTH",
-                "REACT_APP_AWS_COGNITO_COOKIE_EXPIRY_MINS": 55,
-                "REACT_APP_AWS_COGNITO_COOKIE_SECURE": True,
+        TerraformHclModule(
+            self,
+            id="lambda_pipeline",
+            source="git@github.com:deathtumble/terraform_modules.git//modules/lambda_pipeline?ref=v0.4.2",
+            # source="../../../../terraform/modules/lambda_pipeline",
+            variables={
+                "application_name": config.app_name,
+                "destination_builds_bucket_name": self.shared.destination_builds_bucket_name,
+                "function_names": [lambda_function.function_name],
+                "function_arns": [lambda_function.arn],
+                "aws_sns_topic_env_build_notification_name": self.shared.aws_sns_topic_env_build_notification_name,
+                "aws_account_id": self.accounts.aws_account_id
             }
-            TerraformLocal(self, "web_config", web_config)
+        )
 
-            TerraformHclModule(
-                self,
-                id="web",
-                source="git@github.com:deathtumble/terraform_modules.git//modules/web?ref=v0.4.1",
-                variables={
-                    "aws_profile": self.shared.aws_profile,
-                    "fqdn": f'web{self.shared.fqdn}',
-                    "fqdn_no_app": self.shared.environment_domain_name,
-                    "web_acl_name": self.shared.web_acl_name,
-                    "application_name": "releasable",
-                    "environment_config": web_config
+        acm_cert = environment_certificate(
+            self, self.aws_global_provider, self.shared.environment_domain_name
+        )
+
+        api_gateway_module = TerraformHclModule(
+            self,
+            id="api_gateway",
+            source="git@github.com:deathtumble/terraform_modules.git//modules/api_gateway?ref=v0.4.2",
+            # source="../../../../terraform/modules/api_gateway",
+            variables={
+                "aws_region": config.aws_region,
+                "fqdn": self.shared.fqdn,
+                "zone_id": route_53_zone.id,
+                "web_acl_id": aws_wafregional_web_acl_main.id,
+                "certificate_arn": acm_cert.arn,
+                "lambda_invoke_arn": lambda_function.invoke_arn
+            }
+        )
+
+        terraform_build_artifact_key = f'builds/{config.app_name}/refs/branch/{self.environment}/terraform.zip'
+        TerraformLocal(self, "terraform_build_artifact_key",
+                        terraform_build_artifact_key)
+        build_artifact_key = f'builds/{config.app_name}/refs/branch/{self.environment}/cloudfront.zip'
+        TerraformLocal(self, "build_artifact_key", build_artifact_key)
+
+        TerraformHclModule(
+            self,
+            id="cloudfront_pipeline",
+            source="git@github.com:deathtumble/terraform_modules.git//modules/cloudfront_pipeline?ref=v0.1.42",
+            variables={
+                "fqdn": f'web{self.shared.fqdn}',
+                "destination_builds_bucket_name": self.shared.destination_builds_bucket_name,
+                "application_name": "releasable",
+                "branch_name": self.environment,
+                "artifacts_bucket_name": self.shared.artifacts_bucket_name,
+                "build_artifact_key": build_artifact_key
+            }
+        )
+
+        web_config = {
+            "REACT_APP_API_ENDPOINT": f"https://releasable.{self.shared.environment_domain_name}/query",
+            "REACT_APP_PRIMARY_SLDN": self.shared.environment_domain_name,
+            "REACT_APP_API_SLDN": self.shared.environment_domain_name,
+            "REACT_APP_SSO_COOKIE_SLDN": self.shared.environment_domain_name,
+            "REACT_APP_AWS_COGNITO_REGION": config.aws_region,
+            "REACT_APP_AWS_COGNITO_IDENTITY_POOL_REGION": config.aws_region,
+            "REACT_APP_AWS_COGNITO_AUTH_FLOW_TYPE": "USER_SRP_AUTH",
+            "REACT_APP_AWS_COGNITO_COOKIE_EXPIRY_MINS": 55,
+            "REACT_APP_AWS_COGNITO_COOKIE_SECURE": True,
+        }
+        TerraformLocal(self, "web_config", web_config)
+
+        TerraformHclModule(
+            self,
+            id="web",
+            source="git@github.com:deathtumble/terraform_modules.git//modules/web?ref=v0.4.1",
+            variables={
+                "aws_profile": self.shared.aws_profile,
+                "fqdn": f'web{self.shared.fqdn}',
+                "fqdn_no_app": self.shared.environment_domain_name,
+                "web_acl_name": self.shared.web_acl_name,
+                "application_name": "releasable",
+                "environment_config": web_config
+            }
+        )
+
+        DataAwsSecretsmanagerSecretVersion(
+            self, id="repo_token", secret_id="repo_token")
+
+        api_gateway_id = api_gateway_module.get_string("api_id")
+
+        LambdaPermission(
+            self,
+            id="lambda_permission_api_gateway",
+            statement_id="releasable-api-gateway-access",
+            function_name="releasable",
+            principal="apigateway.amazonaws.com",
+            action="lambda:InvokeFunction",
+            source_arn=f"arn:aws:execute-api:eu-west-1:481652375433:{api_gateway_id}/*/*/*"
+        )
+
+        lambda_log_access = IamPolicy(
+            self,
+            id="lambda_log_access_policy",
+            name="releasable-lambda-CloudWatchWriteAccess",
+            policy=json.dumps(
+                {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Action": [
+                                "logs:PutLogEvents",
+                                "logs:CreateLogStream"
+                            ],
+                            "Resource": f'arn:aws:logs:{config.aws_region}:{self.accounts.aws_account_id}:log-group:/aws/lambda/{config.app_name}:*',
+                            "Effect":"Allow"
+                        },
+                        {
+                            "Action": [
+                                "xray:PutTraceSegments",
+                                "xray:PutTelemetryRecords"
+                            ],
+                            "Resource": "*",
+                            "Effect": "Allow"
+                        }
+                    ]
                 }
             )
+        )
 
-            DataAwsSecretsmanagerSecretVersion(
-                self, id="repo_token", secret_id="repo_token")
+        IamRolePolicyAttachment(
+            self,
+            id="lambda_log_access_role_policy_attachement",
+            role=lambda_service_role.name,
+            policy_arn=lambda_log_access.arn
+        )
 
-            api_gateway_id = api_gateway_module.get_string("api_id")
-
-            LambdaPermission(
-                self,
-                id="lambda_permission_api_gateway",
-                statement_id="releasable-api-gateway-access",
-                function_name="releasable",
-                principal="apigateway.amazonaws.com",
-                action="lambda:InvokeFunction",
-                source_arn=f"arn:aws:execute-api:eu-west-1:481652375433:{api_gateway_id}/*/*/*"
-            )
-
-            lambda_log_access = IamPolicy(
-                self,
-                id="lambda_log_access_policy",
-                name="releasable-lambda-CloudWatchWriteAccess",
-                policy=json.dumps(
-                    {
-                        "Version": "2012-10-17",
-                        "Statement": [
-                            {
-                                "Action": [
-                                    "logs:PutLogEvents",
-                                    "logs:CreateLogStream"
-                                ],
-                                "Resource": f'arn:aws:logs:{config.aws_region}:{self.accounts.aws_account_id}:log-group:/aws/lambda/{config.app_name}:*',
-                                "Effect":"Allow"
-                            },
-                            {
-                                "Action": [
-                                    "xray:PutTraceSegments",
-                                    "xray:PutTelemetryRecords"
-                                ],
-                                "Resource": "*",
-                                "Effect": "Allow"
-                            }
-                        ]
-                    }
-                )
-            )
-
-            IamRolePolicyAttachment(
-                self,
-                id="lambda_log_access_role_policy_attachement",
-                role=lambda_service_role.name,
-                policy_arn=lambda_log_access.arn
-            )
-
-            CloudwatchLogGroup(
-                self,
-                id="lambda_log_group",
-                name=f'/aws/lambda/{lambda_function.function_name}',
-                retention_in_days=14
-            )
+        CloudwatchLogGroup(
+            self,
+            id="lambda_log_group",
+            name=f'/aws/lambda/{lambda_function.function_name}',
+            retention_in_days=14
+        )
 
     def _create_backend(self, config):
         bucket = config.tldn + '.' + \

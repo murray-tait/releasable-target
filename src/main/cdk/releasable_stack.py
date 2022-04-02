00d559a8856bceabb3a914a9d0bb0fb4f1d2f121
Murray Tait <6074789+deathtumble@users.noreply.github.com>
2022-04-02 17:30:58 +0100

    Refactor web site to cdktf

--- src/main/cdk/releasable_stack.py
+++ src/main/cdk/releasable_stack.py
@@ -1,5 +1,16 @@
 import json
+from string import Template
 
+from cdktf_cdktf_provider_aws.eventbridge import (
+    CloudwatchEventTarget,
+    CloudwatchEventRule,
+)
+from cdktf_cdktf_provider_aws.codepipeline import (
+    Codepipeline,
+    CodepipelineArtifactStore,
+    CodepipelineStage,
+    CodepipelineStageAction,
+)
 from cdktf_cdktf_provider_aws.route53 import DataAwsRoute53Zone, Route53Record
 from cdktf_cdktf_provider_aws.cloudfront import (
     CloudfrontDistribution,
@@ -26,8 +37,10 @@ from cdktf_cdktf_provider_aws.iam import (
     DataAwsIamPolicyDocumentStatement,
     DataAwsIamPolicyDocument,
     DataAwsIamPolicyDocumentStatementPrincipals,
+    IamRole,
+    IamRolePolicy,
 )
-from cdktf import TerraformHclModule, TerraformStack
+from cdktf import TerraformStack
 from constructs import Construct
 
 from murraytait_cdktf.shared import Shared
@@ -40,6 +53,7 @@ from murraytait_cdktf.env import (
     create_backend,
     api_and_lambda,
     create_lambda_pipeline,
+    create_assume_role_policy,
 )
 
 
@@ -75,9 +89,7 @@ class ReleasableStack(TerraformStack):
             self, aws_global_provider, shared.environment_domain_name
         )
 
-        build_artifact_key = (
-            f"builds/{app_name}/refs/branch/{environment}/cloudfront.zip"
-        )
+        build_artifact_key = f"builds/{app_name}/refs/branch/{environment}/web.zip"
 
         web_config = {
             "REACT_APP_API_ENDPOINT": f"https://releasable.{shared.environment_domain_name}/default",
@@ -92,7 +104,8 @@ class ReleasableStack(TerraformStack):
         }
 
         web_acl_name = shared.web_acl_name
-        self.create_web_site(
+        create_web_site(
+            self,
             "webreleasable",
             web_config,
             f"web{shared.fqdn}",
@@ -102,18 +115,17 @@ class ReleasableStack(TerraformStack):
             acm_cert,
         )
 
-        TerraformHclModule(
-            self,
-            id="cloudfront_pipeline",
-            source="git@github.com:deathtumble/terraform_modules.git//modules/cloudfront_pipeline?ref=v0.1.42",
-            variables={
-                "fqdn": f"web{shared.fqdn}",
-                "destination_builds_bucket_name": shared.destination_builds_bucket_name,
-                "application_name": "releasable",
-                "branch_name": environment,
-                "artifacts_bucket_name": shared.artifacts_bucket_name,
-                "build_artifact_key": build_artifact_key,
-            },
+        artifacts_bucket_name = shared.artifacts_bucket_name
+        destination_builds_bucket_name = shared.destination_builds_bucket_name
+        fqdn = shared.fqdn
+
+        self.create_cloudfront_pipeline(
+            "webreleasable",
+            environment,
+            build_artifact_key,
+            artifacts_bucket_name,
+            destination_builds_bucket_name,
+            f"web{shared.fqdn}",
         )
 
         lambda_name = "releasable"
@@ -140,154 +152,306 @@ class ReleasableStack(TerraformStack):
             environment,
         )
 
-    def create_web_site(
+    def create_cloudfront_pipeline(
         self,
         name,
-        web_config,
+        environment,
+        build_artifact_key,
+        artifacts_bucket_name,
+        destination_builds_bucket_name,
         fqdn,
-        web_acl_name,
-        zone_id,
-        global_provider,
-        acm_cert,
     ):
-        origin_access_identity = CloudfrontOriginAccessIdentity(
+
+        pipeline_role_name = f"{name}-cloudfront-pipeline-CodePipeline"
+        pipeline_role_name_assume_policy = create_assume_role_policy(
+            self, "codepipeline.amazonaws.com", pipeline_role_name
+        )
+
+        pipeline_role = IamRole(
             self,
-            id=f"{name}_origin_access_identity",
-            comment=f"OAI For {fqdn}",
+            id=f"{name}_pipeline_role",
+            name=pipeline_role_name,
+            assume_role_policy=pipeline_role_name_assume_policy.json,
         )
 
-        site_bucket = S3Bucket(
-            self, id=f"{name}_site_bucket", bucket=fqdn, acl="private"
-        )
-
-        S3BucketPublicAccessBlock(
+        pipeline = Codepipeline(
             self,
-            id=f"{name}_site_bucket_public_access_block",
-            bucket=site_bucket.id,
-            block_public_acls=True,
-            block_public_policy=True,
-            restrict_public_buckets=True,
-            ignore_public_acls=True,
+            id=f"{name}_pipeline",
+            name=fqdn,
+            role_arn=pipeline_role.arn,
+            artifact_store=[
+                CodepipelineArtifactStore(location=artifacts_bucket_name, type="S3")
+            ],
+            stage=[
+                CodepipelineStage(
+                    name="Source",
+                    action=[
+                        CodepipelineStageAction(
+                            name="Source",
+                            category="Source",
+                            owner="AWS",
+                            provider="S3",
+                            version="1",
+                            output_artifacts=[f"{name}-source_output"],
+                            configuration={
+                                "S3Bucket": destination_builds_bucket_name,
+                                "S3ObjectKey": build_artifact_key,
+                                "PollForSourceChanges": "true",
+                            },
+                        )
+                    ],
+                ),
+                CodepipelineStage(
+                    name="Deploy",
+                    action=[
+                        CodepipelineStageAction(
+                            name="Deploy",
+                            category="Deploy",
+                            owner="AWS",
+                            provider="S3",
+                            version="1",
+                            input_artifacts=[f"{name}-source_output"],
+                            configuration={"BucketName": fqdn, "Extract": "true"},
+                        )
+                    ],
+                ),
+            ],
         )
 
-        s3_object_access = DataAwsIamPolicyDocumentStatement(
-            sid="s3ObjectAccess",
-            actions=["s3:GetObject"],
-            resources=[f"{site_bucket.arn}/*"],
-            effect="Allow",
-            principals=[
-                DataAwsIamPolicyDocumentStatementPrincipals(
-                    type="AWS",
-                    identifiers=[origin_access_identity.iam_arn],
+        ###########################################################################
+
+        cloudwatch_event_role_name = f"{fqdn}-cloudwatch"
+        cloudwatch_event_assume_role_policy = create_assume_role_policy(
+            self, "events.amazonaws.com", cloudwatch_event_role_name
+        )
+
+        event_role_policy_document = DataAwsIamPolicyDocument(
+            self,
+            id=f"{name}_event_policy_document",
+            statement=[
+                DataAwsIamPolicyDocumentStatement(
+                    sid="xrayWriteAccess",
+                    actions=["codepipeline:StartPipelineExecution"],
+                    resources=[pipeline.arn],
+                    effect="Allow",
                 )
             ],
         )
 
-        s3_list_bucket_access = DataAwsIamPolicyDocumentStatement(
-            sid="s3ListBucketAccess",
-            actions=["s3:ListBucket"],
-            resources=[site_bucket.arn],
-            effect="Allow",
-            principals=[
-                DataAwsIamPolicyDocumentStatementPrincipals(
-                    type="AWS",
-                    identifiers=[origin_access_identity.iam_arn],
-                )
-            ],
+        cloudwatch_event_role = IamRole(
+            self,
+            id=f"{name}_cloudwatch_role",
+            name=cloudwatch_event_role_name,
+            assume_role_policy=cloudwatch_event_assume_role_policy.json,
         )
 
-        config = json.dumps(web_config)
-
-        S3BucketObject(
+        event_role_policy = IamRolePolicy(
             self,
-            id=f"{name}_site_config",
-            bucket=site_bucket.bucket,
-            key="config.js",
-            content=f"window.env = Object.assign({{}}, window.env, {config})",
+            id=f"{name}_codewatch_role_policy",
+            role=cloudwatch_event_role.id,
+            policy=event_role_policy_document.json,
         )
 
-        policy_document = DataAwsIamPolicyDocument(
+        rule = CloudwatchEventRule(
             self,
-            id=f"{name}_site_bucket_policy_document",
-            statement=[s3_object_access, s3_list_bucket_access],
+            id=f"{name}_cloudwatch_event_rule",
+            name=f"{name}_rule",
+            description="Triggers CodePipeline when an a certain object is dropped in a specified S3 bucket",
+            event_pattern=template(
+                "events/cloudwatch_s3_write_trigger_codepipeline.json",
+                {
+                    "bucket_name": destination_builds_bucket_name,
+                    "build_artifact_key": build_artifact_key,
+                },
+            ),
         )
 
-        S3BucketPolicy(
+        CloudwatchEventTarget(
             self,
-            id=f"{name}_site_bucket_policy",
-            bucket=site_bucket.id,
-            policy=policy_document.json,
+            id=f"{name}_cloudwatch_event_target",
+            arn=pipeline.arn,
+            rule=rule.name,
+            role_arn=cloudwatch_event_role.arn,
         )
 
-        waf_acl = DataAwsWafv2WebAcl(
+        CloudwatchEventRule(
             self,
-            id=f"{name}_web_wafv2_acl",
-            name=web_acl_name,
-            scope="CLOUDFRONT",
-            provider=global_provider,
+            id=f"{name}-cloud-pipeline",
+            name=f"{name}-cloud-pipeline",
+            description="Amazon CloudWatch Events rule to automatically start your pipeline when a change occurs in the Amazon S3 object key or S3 folder. Deleting this may prevent changes from being detected in that pipeline. Read more: http://docs.aws.amazon.com/codepipeline/latest/userguide/pipelines-about-starting.html",
+            event_pattern=json.dumps(
+                {
+                    "source": ["aws.s3"],
+                    "detail-type": ["AWS API Call via CloudTrail"],
+                    "detail": {
+                        "eventSource": ["s3.amazonaws.com"],
+                        "eventName": [
+                            "PutObject",
+                            "CompleteMultipartUpload",
+                            "CopyObject",
+                        ],
+                        "requestParameters": {
+                            "bucketName": [f"{destination_builds_bucket_name}"],
+                            "key": [f"{build_artifact_key}"],
+                        },
+                    },
+                }
+            ),
         )
 
-        cloud_front_dist = CloudfrontDistribution(
-            self,
-            id=f"{name}_cloudfront_dist",
-            enabled=True,
-            is_ipv6_enabled=True,
-            comment=f"Cloudfront distribution for {fqdn}",
-            default_root_object="index.html",
-            price_class="PriceClass_100",
-            web_acl_id=waf_acl.arn,
-            origin=[
-                CloudfrontDistributionOrigin(
-                    domain_name=site_bucket.bucket_regional_domain_name,
-                    origin_id=f"{site_bucket.id}-origin",
-                    s3_origin_config=CloudfrontDistributionOriginS3OriginConfig(
-                        origin_access_identity=origin_access_identity.cloudfront_access_identity_path
-                    ),
-                )
-            ],
-            custom_error_response=[
-                CloudfrontDistributionCustomErrorResponse(
-                    error_caching_min_ttl=300,
-                    error_code=404,
-                    response_code=200,
-                    response_page_path="/index.html",
-                )
-            ],
-            aliases=[fqdn],
-            default_cache_behavior=CloudfrontDistributionDefaultCacheBehavior(
-                min_ttl=0,
-                default_ttl=0,
-                max_ttl=0,
-                target_origin_id=f"{site_bucket.id}-origin",
-                viewer_protocol_policy="redirect-to-https",
-                allowed_methods=["GET", "HEAD"],
-                cached_methods=["GET", "HEAD"],
-                forwarded_values=CloudfrontDistributionDefaultCacheBehaviorForwardedValues(
-                    query_string=False,
-                    cookies=CloudfrontDistributionDefaultCacheBehaviorForwardedValuesCookies(
-                        forward="none",
-                    ),
+
+def create_web_site(
+    stack,
+    name,
+    web_config,
+    fqdn,
+    web_acl_name,
+    zone_id,
+    global_provider,
+    acm_cert,
+):
+    origin_access_identity = CloudfrontOriginAccessIdentity(
+        stack,
+        id=f"{name}_origin_access_identity",
+        comment=f"OAI For {fqdn}",
+    )
+
+    site_bucket = S3Bucket(stack, id=f"{name}_site_bucket", bucket=fqdn, acl="private")
+
+    S3BucketPublicAccessBlock(
+        stack,
+        id=f"{name}_site_bucket_public_access_block",
+        bucket=site_bucket.id,
+        block_public_acls=True,
+        block_public_policy=True,
+        restrict_public_buckets=True,
+        ignore_public_acls=True,
+    )
+
+    s3_object_access = DataAwsIamPolicyDocumentStatement(
+        sid="s3ObjectAccess",
+        actions=["s3:GetObject"],
+        resources=[f"{site_bucket.arn}/*"],
+        effect="Allow",
+        principals=[
+            DataAwsIamPolicyDocumentStatementPrincipals(
+                type="AWS",
+                identifiers=[origin_access_identity.iam_arn],
+            )
+        ],
+    )
+
+    s3_list_bucket_access = DataAwsIamPolicyDocumentStatement(
+        sid="s3ListBucketAccess",
+        actions=["s3:ListBucket"],
+        resources=[site_bucket.arn],
+        effect="Allow",
+        principals=[
+            DataAwsIamPolicyDocumentStatementPrincipals(
+                type="AWS",
+                identifiers=[origin_access_identity.iam_arn],
+            )
+        ],
+    )
+
+    config = json.dumps(web_config)
+
+    S3BucketObject(
+        stack,
+        id=f"{name}_site_config",
+        bucket=site_bucket.bucket,
+        key="config.js",
+        content=f"window.env = Object.assign({{}}, window.env, {config})",
+    )
+
+    policy_document = DataAwsIamPolicyDocument(
+        stack,
+        id=f"{name}_site_bucket_policy_document",
+        statement=[s3_object_access, s3_list_bucket_access],
+    )
+
+    S3BucketPolicy(
+        stack,
+        id=f"{name}_site_bucket_policy",
+        bucket=site_bucket.id,
+        policy=policy_document.json,
+    )
+
+    waf_acl = DataAwsWafv2WebAcl(
+        stack,
+        id=f"{name}_web_wafv2_acl",
+        name=web_acl_name,
+        scope="CLOUDFRONT",
+        provider=global_provider,
+    )
+
+    cloud_front_dist = CloudfrontDistribution(
+        stack,
+        id=f"{name}_cloudfront_dist",
+        enabled=True,
+        is_ipv6_enabled=True,
+        comment=f"Cloudfront distribution for {fqdn}",
+        default_root_object="index.html",
+        price_class="PriceClass_100",
+        web_acl_id=waf_acl.arn,
+        origin=[
+            CloudfrontDistributionOrigin(
+                domain_name=site_bucket.bucket_regional_domain_name,
+                origin_id=f"{site_bucket.id}-origin",
+                s3_origin_config=CloudfrontDistributionOriginS3OriginConfig(
+                    origin_access_identity=origin_access_identity.cloudfront_access_identity_path
+                ),
+            )
+        ],
+        custom_error_response=[
+            CloudfrontDistributionCustomErrorResponse(
+                error_caching_min_ttl=300,
+                error_code=404,
+                response_code=200,
+                response_page_path="/index.html",
+            )
+        ],
+        aliases=[fqdn],
+        default_cache_behavior=CloudfrontDistributionDefaultCacheBehavior(
+            min_ttl=0,
+            default_ttl=0,
+            max_ttl=0,
+            target_origin_id=f"{site_bucket.id}-origin",
+            viewer_protocol_policy="redirect-to-https",
+            allowed_methods=["GET", "HEAD"],
+            cached_methods=["GET", "HEAD"],
+            forwarded_values=CloudfrontDistributionDefaultCacheBehaviorForwardedValues(
+                query_string=False,
+                cookies=CloudfrontDistributionDefaultCacheBehaviorForwardedValuesCookies(
+                    forward="none",
                 ),
             ),
-            restrictions=CloudfrontDistributionRestrictions(
-                geo_restriction=CloudfrontDistributionRestrictionsGeoRestriction(
-                    restriction_type="whitelist",
-                    locations=["GB", "IE"],
-                )
-            ),
-            viewer_certificate=CloudfrontDistributionViewerCertificate(
-                acm_certificate_arn=acm_cert.arn,
-                ssl_support_method="sni-only",
-                minimum_protocol_version="TLSv1.2_2018",
-            ),
-        )
+        ),
+        restrictions=CloudfrontDistributionRestrictions(
+            geo_restriction=CloudfrontDistributionRestrictionsGeoRestriction(
+                restriction_type="whitelist",
+                locations=["GB", "IE"],
+            )
+        ),
+        viewer_certificate=CloudfrontDistributionViewerCertificate(
+            acm_certificate_arn=acm_cert.arn,
+            ssl_support_method="sni-only",
+            minimum_protocol_version="TLSv1.2_2018",
+        ),
+    )
 
-        Route53Record(
-            self,
-            id=f"{name}_site_route_53_record",
-            zone_id=zone_id,
-            name=fqdn,
-            type="CNAME",
-            ttl=300,
-            records=[cloud_front_dist.domain_name],
-        )
+    Route53Record(
+        stack,
+        id=f"{name}_site_route_53_record",
+        zone_id=zone_id,
+        name=fqdn,
+        type="CNAME",
+        ttl=300,
+        records=[cloud_front_dist.domain_name],
+    )
+
+
+def template(template_file_name, dict):
+    with open(template_file_name, "r") as file:
+        policy = Template(file.read()).substitute(dict)
+    return policy
